338349e8935d0d168c4d049a3a018037ff3d0420
Vineeth <you@example.com>
2024-10-09 10:59:15 +0530

    Mus

--- 240122-mrsshika-jojo/data.js
+++ 240122-mrsshika-jojo/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-road",
-      "name": "Road",
+      "id": "0-reception",
+      "name": "Reception",
       "levels": [
         {
           "tileSize": 256,
@@ -24,29 +24,23 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.8619004079716746,
-        "pitch": 0.007587568170102088,
-        "fov": 1.2828048894984105
+        "yaw": 1.564794289548205,
+        "pitch": 0.0014154279511036094,
+        "fov": 1.3900591270580378
       },
       "linkHotspots": [
         {
-          "yaw": 1.9909159378455668,
-          "pitch": 0.08060077140063271,
-          "rotation": 0,
-          "target": "1-ss-v01"
-        },
-        {
-          "yaw": 1.2062991038393989,
-          "pitch": 0.038697145433737035,
-          "rotation": 0,
-          "target": "8-ps-v01"
+          "yaw": 1.5049059850716446,
+          "pitch": 0.05235050760562565,
+          "rotation": 3.141592653589793,
+          "target": "1-hair-cutting-entrance"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-ss-v01",
-      "name": "SS-V01",
+      "id": "1-hair-cutting-entrance",
+      "name": "Hair Cutting Entrance",
       "levels": [
         {
           "tileSize": 256,
@@ -68,29 +62,85 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.7927862838762856,
-        "pitch": 0.006843688937721026,
-        "fov": 1.2828048894984105
+        "yaw": 1.5004341824298812,
+        "pitch": 0.0018509442437508739,
+        "fov": 1.3900591270580378
       },
       "linkHotspots": [
         {
-          "yaw": -0.16594843626217148,
-          "pitch": -0.07391101319174886,
+          "yaw": 1.4140277357434847,
+          "pitch": 0.1229250940972797,
+          "rotation": 3.141592653589793,
+          "target": "2-hair-cutting-v-1"
+        },
+        {
+          "yaw": -2.2069900878280837,
+          "pitch": 0.052495997666905936,
+          "rotation": 3.141592653589793,
+          "target": "0-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hair-cutting-v-1",
+      "name": "Hair Cutting V-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.4887940310921657,
+        "pitch": -7.896448330768635e-7,
+        "fov": 1.3900591270580378
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.824481468025253,
+          "pitch": 0.0776810444333389,
+          "rotation": 3.141592653589793,
+          "target": "3-hair-cutting-v-2"
+        },
+        {
+          "yaw": -2.69670077653484,
+          "pitch": 0.12535817110493852,
+          "rotation": 3.141592653589793,
+          "target": "1-hair-cutting-entrance"
+        },
+        {
+          "yaw": -0.8734832771196839,
+          "pitch": 0.1502457106889299,
+          "rotation": 3.141592653589793,
+          "target": "5-hair-cutting-v-3"
+        },
+        {
+          "yaw": -2.089945931817354,
+          "pitch": 0.21042092863643802,
           "rotation": 0,
-          "target": "0-road"
-        },
-        {
-          "yaw": 1.73002389175155,
-          "pitch": 0.20182991492324476,
-          "rotation": 3.141592653589793,
-          "target": "2-ss-v02"
+          "target": "4-wash-area"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-ss-v02",
-      "name": "SS-V02",
+      "id": "3-hair-cutting-v-2",
+      "name": "Hair Cutting V-2",
       "levels": [
         {
           "tileSize": 256,
@@ -112,29 +162,23 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 2.6632445832766463,
-        "pitch": -0.01336441613610262,
-        "fov": 1.2828048894984105
+        "yaw": 1.5459507012682838,
+        "pitch": 0.0037018884875053004,
+        "fov": 1.3900591270580378
       },
       "linkHotspots": [
         {
-          "yaw": 1.1129199382707071,
-          "pitch": 0.13603399696761898,
+          "yaw": 1.472735285376011,
+          "pitch": 0.2447374413927612,
           "rotation": 3.141592653589793,
-          "target": "3-ss-v03"
-        },
-        {
-          "yaw": -2.9564778836872883,
-          "pitch": 0.15584709423305654,
-          "rotation": 3.141592653589793,
-          "target": "1-ss-v01"
+          "target": "2-hair-cutting-v-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-ss-v03",
-      "name": "SS-V03",
+      "id": "4-wash-area",
+      "name": "Wash Area",
       "levels": [
         {
           "tileSize": 256,
@@ -156,79 +200,23 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.0417668482969091,
-        "pitch": -0.0011379475844393028,
-        "fov": 1.2828048894984105
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.1626009612961603,
-          "pitch": 0.04995335720481542,
-          "rotation": 3.141592653589793,
-          "target": "4-ss-v04"
-        },
-        {
-          "yaw": 0.5658783845467408,
-          "pitch": 0.1420094055184471,
-          "rotation": 0,
-          "target": "5-ss-v05"
-        },
-        {
-          "yaw": -3.135583951564101,
-          "pitch": 0.19018073094721188,
-          "rotation": 3.141592653589793,
-          "target": "2-ss-v02"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-ss-v04",
-      "name": "SS-V04",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 1.6027486330288827,
+        "yaw": 1.3709575940088516,
         "pitch": 0,
-        "fov": 1.2828048894984105
+        "fov": 1.3900591270580378
       },
       "linkHotspots": [
         {
-          "yaw": -2.04377271947158,
-          "pitch": 0.08026814803939786,
-          "rotation": 3.141592653589793,
-          "target": "3-ss-v03"
-        },
-        {
-          "yaw": 1.268223896371003,
-          "pitch": 0.09983254625091931,
+          "yaw": 2.5500125789629635,
+          "pitch": 0.18552203588990324,
           "rotation": 0,
-          "target": "7-pantry"
+          "target": "2-hair-cutting-v-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-ss-v05",
-      "name": "SS-V05",
+      "id": "5-hair-cutting-v-3",
+      "name": "Hair Cutting V-3",
       "levels": [
         {
           "tileSize": 256,
@@ -250,29 +238,35 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.5512062481450979,
-        "pitch": 0,
-        "fov": 1.2828048894984105
+        "yaw": 0.9775281751284552,
+        "pitch": -0.0018509442437384394,
+        "fov": 1.3900591270580378
       },
       "linkHotspots": [
         {
-          "yaw": 1.86455363829909,
-          "pitch": 0.09776552007161499,
-          "rotation": 3.141592653589793,
-          "target": "6-ss-v06"
+          "yaw": -2.630959855416947,
+          "pitch": 0.14982262483261977,
+          "rotation": 0,
+          "target": "6-spa-room"
         },
         {
-          "yaw": -0.6656335216385365,
-          "pitch": -0.007587568170094983,
+          "yaw": 3.0695481903703667,
+          "pitch": 0.13098015861829992,
           "rotation": 0,
-          "target": "3-ss-v03"
+          "target": "7-pedicure-room"
+        },
+        {
+          "yaw": 0.6342552949244258,
+          "pitch": 0.12717775051741853,
+          "rotation": 3.141592653589793,
+          "target": "2-hair-cutting-v-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-ss-v06",
-      "name": "SS-V06",
+      "id": "6-spa-room",
+      "name": "Spa Room",
       "levels": [
         {
           "tileSize": 256,
@@ -294,23 +288,23 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.9190651739062368,
-        "pitch": -0.0025291893900245555,
-        "fov": 1.2828048894984105
+        "yaw": 1.525761564329894,
+        "pitch": 0.0010096059511361943,
+        "fov": 1.3900591270580378
       },
       "linkHotspots": [
         {
-          "yaw": 1.548362660428383,
-          "pitch": 0.1448241042447691,
-          "rotation": 3.141592653589793,
-          "target": "5-ss-v05"
+          "yaw": 2.7146578357422015,
+          "pitch": 0.17266189006872423,
+          "rotation": 0,
+          "target": "5-hair-cutting-v-3"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-pantry",
-      "name": "Pantry",
+      "id": "7-pedicure-room",
+      "name": "Pedicure Room",
       "levels": [
         {
           "tileSize": 256,
@@ -332,310 +326,22 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 1.3317534995376263,
-        "pitch": -0.0025342696833021705,
-        "fov": 1.2828048894984105
+        "yaw": 1.4089568411477966,
+        "pitch": -0.0010097933139654458,
+        "fov": 1.3900591270580378
       },
       "linkHotspots": [
         {
-          "yaw": 2.1322730393435805,
-          "pitch": 0.07193350660416797,
+          "yaw": -0.4060030124119933,
+          "pitch": 0.05466448967330706,
           "rotation": 0,
-          "target": "4-ss-v04"
-        },
-        {
-          "yaw": -2.899963048290232,
-          "pitch": 0.0733895220767593,
-          "rotation": 0,
-          "target": "13-packing-room"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "8-ps-v01",
-      "name": "PS-V01",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 1.204381867542967,
-        "pitch": 0.010123951931127806,
-        "fov": 1.2828048894984105
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.521476200109113,
-          "pitch": 0.2886719350126796,
-          "rotation": 3.141592653589793,
-          "target": "9-ps-v02"
-        },
-        {
-          "yaw": -0.7097353154045969,
-          "pitch": 0.007594762541096145,
-          "rotation": 0,
-          "target": "0-road"
-        },
-        {
-          "yaw": 1.7675559470633226,
-          "pitch": 0.2486573110764283,
-          "rotation": 3.141592653589793,
-          "target": "12-ps-v05"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "9-ps-v02",
-      "name": "PS-V02",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 1.6450705496942248,
-        "pitch": -0.015185596515165045,
-        "fov": 1.2828048894984105
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.8423005013615859,
-          "pitch": 0.14546537036645368,
-          "rotation": 3.141592653589793,
-          "target": "10-ps-v03"
-        },
-        {
-          "yaw": -1.2446128529245062,
-          "pitch": 0.07951921764280101,
-          "rotation": 3.141592653589793,
-          "target": "8-ps-v01"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "10-ps-v03",
-      "name": "PS-V03",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 1.5484535218992281,
-        "pitch": 0.005051164410826203,
-        "fov": 1.2828048894984105
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.6139212797883324,
-          "pitch": 0.12426649978034199,
-          "rotation": 0,
-          "target": "13-packing-room"
-        },
-        {
-          "yaw": 2.5846870194992118,
-          "pitch": 0.1413356770102041,
-          "rotation": 3.141592653589793,
-          "target": "11-ps-v04"
-        },
-        {
-          "yaw": 1.0721143108748805,
-          "pitch": 0.16286522864835185,
-          "rotation": 3.141592653589793,
-          "target": "9-ps-v02"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "11-ps-v04",
-      "name": "PS-V04",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 2.0134896431047355,
-        "pitch": 0.008964688431003509,
-        "fov": 1.2828048894984105
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.3379953971779948,
-          "pitch": 0.18805047406572406,
-          "rotation": 3.141592653589793,
-          "target": "12-ps-v05"
-        },
-        {
-          "yaw": 0.8993427642146052,
-          "pitch": 0.1348739465848663,
-          "rotation": 3.141592653589793,
-          "target": "10-ps-v03"
-        },
-        {
-          "yaw": -0.045788812489814035,
-          "pitch": 0.09476032955214997,
-          "rotation": 0,
-          "target": "13-packing-room"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "12-ps-v05",
-      "name": "PS-V05",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -2.8791690444587754,
-        "pitch": -0.0059780840127778845,
-        "fov": 1.2828048894984105
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.5214937988590727,
-          "pitch": 0.1422949078381741,
-          "rotation": 3.141592653589793,
-          "target": "8-ps-v01"
-        },
-        {
-          "yaw": 0.8540967977062799,
-          "pitch": 0.14886913904789978,
-          "rotation": 3.141592653589793,
-          "target": "11-ps-v04"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "13-packing-room",
-      "name": "Packing Room",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 2.852405680512808,
-        "pitch": 0.017719622816843028,
-        "fov": 1.2828048894984105
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.0446872554314837,
-          "pitch": 0.030350272680397694,
-          "rotation": 0,
-          "target": "10-ps-v03"
-        },
-        {
-          "yaw": 1.9896410526260135,
-          "pitch": 0.11898738806203113,
-          "rotation": 0,
-          "target": "7-pantry"
+          "target": "5-hair-cutting-v-3"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "240122-Mrs.SHIKA JOJO",
+  "name": "240151_muskan_beauty_salon",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
